5d4c2ed5642ac343b3f096d16cf59821fc112f95
Sakil Khan <mdsakilkhan649@gmail.com>
2025-12-12 06:53:24 +0600

    Fainaly Ui design Conpleted

--- lib/widgets/Custom_Dailog.dart
+++ lib/widgets/Custom_Dailog.dart
@@ -19,134 +19,137 @@ class Customdialog extends StatelessWidget {
       backgroundColor: Colors.white,
       shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20)),
       child: CustomBox(
-        height: 370.h,
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            Column(
-              mainAxisAlignment: MainAxisAlignment.start,
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Align(
-                  alignment: Alignment.topRight,
-                  child: InkWell(
-                    onTap: () {
-                      Get.back();
-                    },
-                    child: SvgPicture.asset(
-                      AppIcons.crosx_icon,
-                      height: 18.h,
-                      width: 18.w,
-                    ),
-                  ),
-                ),
-                SizedBox(height: 10.h),
-                Row(
-                  children: [
-                    Column(
-                      mainAxisAlignment: MainAxisAlignment.start,
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        CustomText(text: "Meal Name", color: Colors.white),
-                        SizedBox(height: 5.h),
-                        Container(
-                          height: 32.h,
-                          width: 130.w,
-                          child: Customtextfield(
-                            controller: weigthContoller,
-                            hintText: "Type.. ",
-                            obscureText: false,
-                            textInputType: TextInputType.text,
-                            contentPadding: EdgeInsets.symmetric(
-                              horizontal: 10,
-                              vertical: 0,
-                            ),
-                          ),
-                        ),
-                      ],
-                    ),
-                    SizedBox(width: 10.w),
-
-                    Column(
-                      mainAxisAlignment: MainAxisAlignment.start,
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        CustomText(text: "Food Name", color: Colors.white),
-                        SizedBox(height: 5.h),
-                        Container(
-                          height: 32.h,
-                          width: 130.w,
-                          child: Customtextfield(
-                            controller: weigthContoller,
-                            hintText: "Type.. ",
-                            obscureText: false,
-                            textInputType: TextInputType.text,
-                            contentPadding: EdgeInsets.symmetric(
-                              horizontal: 10,
-                              vertical: 0,
-                            ),
-                          ),
-                        ),
-                      ],
-                    ),
-                  ],
-                ),
-                SizedBox(height: 10.h),
-                Row(
-                  children: [
-                    Column(
-                      mainAxisAlignment: MainAxisAlignment.start,
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        CustomText(text: "Amount", color: Colors.white),
-                        SizedBox(height: 5.h),
-                        Container(
-                          height: 32.h,
-                          width: 130.w,
-                          child: Customtextfield(
-                            controller: weigthContoller,
-                            hintText: "Type.. ",
-                            obscureText: false,
-                            textInputType: TextInputType.text,
-                            contentPadding: EdgeInsets.symmetric(
-                              horizontal: 10,
-                              vertical: 0,
-                            ),
-                          ),
-                        ),
-                      ],
-                    ),
-                  ],
-                ),
-                SizedBox(height: 10.h),
-
-                ///==============Submit Button========================
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Container(
-                      height: 48.h,
-                      width: 130.w,
-                      child: CustomButton(
-                        text: "Cancel",
-                        backgroundColor: AppColors.blue600,
-                        onPressed: () {
-                          Get.back();
-                        },
+        height: 450.w,
+        child: Padding(
+          padding: EdgeInsets.fromLTRB(30, 20, 20, 20),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Align(
+                    alignment: Alignment.topRight,
+                    child: InkWell(
+                      onTap: () {
+                        Get.back();
+                      },
+                      child: SvgPicture.asset(
+                        AppIcons.crosx_icon,
+                        height: 18.h,
+                        width: 18.w,
                       ),
                     ),
-                    Container(
-                      height: 48.h,
-                      width: 130.w,
-                      child: CustomButton(text: "Add Meal", onPressed: () {}),
-                    ),
-                    SizedBox(height: 170.h),
-                  ],
-                ),
-              ],
-            ),
-          ],
+                  ),
+                  SizedBox(height: 10.h),
+                  Row(
+                    children: [
+                      Column(
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          CustomText(text: "Meal Name", color: Colors.white),
+                          SizedBox(height: 5.h),
+                          Container(
+                            height: 32.h,
+                            width: 130.w,
+                            child: Customtextfield(
+                              controller: weigthContoller,
+                              hintText: "Type.. ",
+                              obscureText: false,
+                              textInputType: TextInputType.text,
+                              contentPadding: EdgeInsets.symmetric(
+                                horizontal: 10,
+                                vertical: 0,
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                      SizedBox(width: 10.w),
+
+                      Column(
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          CustomText(text: "Food Name", color: Colors.white),
+                          SizedBox(height: 5.h),
+                          Container(
+                            height: 32.h,
+                            width: 130.w,
+                            child: Customtextfield(
+                              controller: weigthContoller,
+                              hintText: "Type.. ",
+                              obscureText: false,
+                              textInputType: TextInputType.text,
+                              contentPadding: EdgeInsets.symmetric(
+                                horizontal: 10,
+                                vertical: 0,
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                  SizedBox(height: 10.h),
+                  Row(
+                    children: [
+                      Column(
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          CustomText(text: "Amount", color: Colors.white),
+                          SizedBox(height: 5.h),
+                          Container(
+                            height: 32.h,
+                            width: 130.w,
+                            child: Customtextfield(
+                              controller: weigthContoller,
+                              hintText: "Type.. ",
+                              obscureText: false,
+                              textInputType: TextInputType.text,
+                              contentPadding: EdgeInsets.symmetric(
+                                horizontal: 10,
+                                vertical: 0,
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                  SizedBox(height: 10.h),
+
+                  ///==============Submit Button========================
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Container(
+                        height: 48.h,
+                        width: 130.w,
+                        child: CustomButton(
+                          text: "Cancel",
+                          backgroundColor: AppColors.blue600,
+                          onPressed: () {
+                            Get.back();
+                          },
+                        ),
+                      ),
+                      Container(
+                        height: 48.h,
+                        width: 130.w,
+                        child: CustomButton(text: "Add Meal", onPressed: () {}),
+                      ),
+                      SizedBox(height: 170.h),
+                    ],
+                  ),
+                ],
+              ),
+            ],
+          ),
         ),
       ),
     );
